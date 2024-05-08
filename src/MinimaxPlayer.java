fbaf0a14c37bca000a4fafef3c9c15af1a87951e
seiv324 <stefan.habringer@gmail.com>
2024-05-08 12:52:00 +0000

    Disabled Pruning for heuristic ordering

--- src/MinimaxPlayer.java
+++ src/MinimaxPlayer.java
@@ -165,10 +165,6 @@ final class MinimaxCalculator {
         moveCount++;
         int strength = expandMinNode(depth - 1, maxStrength);
 
-        if (strength > parentMinimum) {
-          board.undoLastMove();
-          return strength;
-        }
         if (strength > maxStrength) {
           maxStrength = strength;
         }
@@ -202,10 +198,6 @@ final class MinimaxCalculator {
         moveCount++;
         int strength = expandMaxNode(depth - 1, minStrength);
 
-        if (strength < parentMaximum) {
-          board.undoLastMove();
-          return strength;
-        }
         if (strength < minStrength) {
           minStrength = strength;
         }
